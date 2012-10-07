34662e7d44f79beb98162097a11c41e0b7bc9ae1
succesom <omelchenko_ig@mail.ru>
2012-10-07 21:21:14 +0300

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
   def home
+   @tittle = 'Home'
   end
 
   def contract
+   @tittle = 'Contact'
+  end
+  def about
+   @tittle = 'About'
   end
 end
